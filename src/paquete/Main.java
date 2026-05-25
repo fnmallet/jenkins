2aae662ac0d29bcfe6f67420a9eb5353cd1e35f2
Fernando Mallet <fernandomallet@outlook.com>
2026-05-25 00:24:21 -0300

    Jenkins

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -1,9 +1,16 @@
 package paquete;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!!!");
+		
+		System.out.println("Presioná Enter para cerrar...");
+		Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        scanner.close();
 	}
 
 }
